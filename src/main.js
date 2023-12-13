84cc5cc09747a183a1d9b697e59d03d0b3202632
Tom Zhong <tom.zhong.cn@gmail.com>
2023-12-13 10:42:29 +1030

    add vue router and create route /map

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import Dashboard from "./pages/Dashboard.vue";
+import Map from "./pages/Map.vue";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", name: "Home", component: Dashboard },
+  { path: "/map", name: "About", component: Map },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+});
+
+const app = createApp(App);
+app.use(router);
+
+app.mount("#app");
