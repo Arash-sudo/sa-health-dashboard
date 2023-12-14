7b24ce370363ec2f0849bc73f47569a9eba8e1f1
thushan <thushan@oneworldled.com.au>
2023-12-14 17:04:04 +1030

    made the ambulances moving

--- src/components/map/MapView.vue
+++ src/components/map/MapView.vue
@@ -13,6 +13,14 @@
       >
         <l-tooltip>{{ hospital.name }}</l-tooltip>
       </l-marker>
+      <l-marker
+        v-for="ambulance in ambulanceCords"
+        :key="ambulance.id"
+        :lat-lng="ambulance.cord"
+        :icon="ambulanceIcon"
+      >
+        <l-tooltip>Ambulance</l-tooltip>
+      </l-marker>
     </l-map>
   </div>
 </template>
@@ -29,6 +37,7 @@ import {
 } from "@vue-leaflet/vue-leaflet";
 import { HOSPITAL_CORDINANTS } from "../../constants";
 import hospitalIcon from "./assets/hospital.svg";
+import ambulanceIcon from "./assets/ambulance.svg";
 
 export default {
   components: {
@@ -47,10 +56,56 @@ export default {
         iconUrl: hospitalIcon,
         iconSize: [30, 30],
       }),
+      ambulanceCords: [], // Initialize ambulance coordinates array
+      ambulanceIcon: L.icon({
+        iconUrl: ambulanceIcon,
+        iconSize: [30, 30],
+      }),
     };
   },
-  // mounted(){
-  // }
+  mounted() {
+    // Generate ambulance coordinates
+    this.generateInitialAmbulanceCoordinates();
+    // This part will update the coordinates every second, if it's slow, increase later on
+    setInterval(() => {
+      this.updateAmbulanceCoordinates();
+    }, 1000);
+  },
+  methods: {
+    generateInitialAmbulanceCoordinates() {
+      const minLatitude = -34.8800;
+      const maxLatitude = -35.1000;
+      const minLongitude = 138.5646;
+      const maxLongitude = 138.6000;
+
+      for (let i = 0; i < 4; i++) {
+        const latitude = this.getRandomNumber(minLatitude, maxLatitude);
+        const longitude = this.getRandomNumber(minLongitude, maxLongitude);
+
+        this.ambulanceCords.push({ id: i + 1, cord: [latitude, longitude] });
+      }
+    },
+
+    updateAmbulanceCoordinates() {
+      // Increment coordinates for each ambulance
+      const latitudeIncrement = 0.0003;
+      const longitudeIncrement = 0.0002;
+
+      this.ambulanceCords = this.ambulanceCords.map((ambulance) => {
+        return {
+          ...ambulance,
+          cord: [
+            ambulance.cord[0] + latitudeIncrement,
+            ambulance.cord[1] + longitudeIncrement,
+          ],
+        };
+      });
+    },
+
+    getRandomNumber(min, max) {
+      return Math.random() * (max - min) + min;
+    },
+  },
 };
 </script>
 
