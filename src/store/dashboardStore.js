d69c801f66f23f3c2c1b35e54514583af5a14a2e
Tom Zhong <tom.zhong.cn@gmail.com>
2023-12-15 12:52:26 +1030

    set up pinia store

--- src/store/dashboardStore.js
+++ src/store/dashboardStore.js
@@ -1,3 +1,23 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
-export const useDashboardStore = defineStore("dashboard", {});
+export const useDashboardStore = defineStore("dashboard", () => {
+  const dashboard = ref([]);
+  const selectedHospitalID = ref("01");
+
+  const selectedHospitalData = computed(() => {
+    if (dashboard.value.length === 0) return {};
+    return dashboard.value.find(
+      (hospital) => hospital.id === selectedHospitalID.value
+    );
+  });
+  const setDashboard = (data) => {
+    dashboard.value = data;
+  };
+
+  const selectHospital = (id) => {
+    selectedHospitalID.value = id;
+  };
+
+  return { dashboard, setDashboard, selectHospital, selectedHospitalData };
+});
