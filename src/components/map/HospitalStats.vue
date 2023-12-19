3b5585e52a47abb86f0d01b6c19bc814d72320b7
Tom Zhong <tom.zhong.cn@gmail.com>
2023-12-19 12:18:45 +1030

    hospital stats layout improvement trial 01

--- src/components/map/HospitalStats.vue
+++ src/components/map/HospitalStats.vue
@@ -1,29 +1,32 @@
 <template>
   <div v-if="!isDataReady">loading</div>
   <div class="container" v-if="isDataReady">
-    <div class="title">
-      Real-time data for <br />
-      <div class="hospital-name">{{ selectedHospitalData.name }}</div>
-    </div>
-
-    <div>Address: {{ selectedHospitalData.location.address }}</div>
-    <div>Coordinates: {{ selectedHospitalData.location.cord }}</div>
-
-    <div class="highlights">
-      <div>Emergency Utilization:</div>
-      <div>
-        {{
-          (
-            (Number(selectedHospitalData.ed[0].patients) /
-              Number(selectedHospitalData.ed[0].capacity)) *
-            100
-          ).toFixed(2)
-        }}%
+    <div class="header">
+      <div class="info">
+        <div class="title">
+          Real-time data for <br />
+          <div class="hospital-name">{{ selectedHospitalData.name }}</div>
+        </div>
+        <div>Address: {{ selectedHospitalData.location.address }}</div>
+        <div>Coordinates: {{ selectedHospitalData.location.cord }}</div>
       </div>
-      <div>Expecting Ambulance:</div>
-      <div>{{ selectedHospitalData.ed[0].expecting }}</div>
-      <div>Clearing Estimate:</div>
-      <div>{{ selectedHospitalData.ambo[0].time }} min</div>
+
+      <div class="highlights">
+        <div>Emergency Utilization:</div>
+        <div>
+          {{
+            (
+              (Number(selectedHospitalData.ed[0].patients) /
+                Number(selectedHospitalData.ed[0].capacity)) *
+              100
+            ).toFixed(2)
+          }}%
+        </div>
+        <div>Expecting Ambulance:</div>
+        <div>{{ selectedHospitalData.ed[0].expecting }}</div>
+        <div>Clearing Estimate:</div>
+        <div>{{ selectedHospitalData.ambo[0].time }} min</div>
+      </div>
     </div>
 
     <div class="live-feed-title">
@@ -55,36 +58,58 @@ const { selectedHospitalData, isDataReady } = storeToRefs(useDashboardStore());
 
 <style scoped>
 .container {
-  margin: 0.5rem;
-  padding-inline: 2rem;
+  /* margin: 1rem; */
+  padding: 2rem;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 10px;
 }
 
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
 .title {
-  font-size: 1.5rem;
-  color: cornsilk;
+  font-size: 2rem;
+  color: #333;
+  font-weight: bold;
 }
 
 .title .hospital-name {
-  font-size: 2rem;
+  color: #0056b3;
+  font-weight: 600;
 }
 
 .highlights {
-  padding-inline: 3rem;
-  font-size: 2rem;
-  color: aliceblue;
+  padding: 2rem;
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: repeat(2, minmax(200px, 1fr));
+  gap: 1rem;
+}
+
+.highlights > div {
+  background: #e7f1ff;
+  padding: 0.3rem;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  color: #333;
+}
+
+.highlights > div:nth-child(2n) {
+  background-color: bisque;
 }
 
 .live-feed-container {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(2, minmax(300px, 1fr));
+  gap: 1rem;
+  margin-top: 2rem;
 }
 
 .dashboard-container {
   width: 80%;
   align-items: center;
   display: inline-block;
-  margin-top: 1.5rem;
 }
 </style>
