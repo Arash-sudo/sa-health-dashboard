74956b2a16de99cd88a648ff73e1a1c10e147c98
Tom Zhong <tom.zhong.cn@gmail.com>
2023-12-19 12:52:42 +1030

    2nd try on hospitalstats component

--- src/components/map/HospitalStats.vue
+++ src/components/map/HospitalStats.vue
@@ -10,28 +10,34 @@
         <div>Address: {{ selectedHospitalData.location.address }}</div>
         <div>Coordinates: {{ selectedHospitalData.location.cord }}</div>
       </div>
+      <img :src="logo" alt="healux logo" class="logo" />
+    </div>
 
-      <div class="highlights">
-        <div>Emergency Utilization:</div>
-        <div>
-          {{
-            (
-              (Number(selectedHospitalData.ed[0].patients) /
-                Number(selectedHospitalData.ed[0].capacity)) *
-              100
-            ).toFixed(2)
-          }}%
-        </div>
-        <div>Expecting Ambulance:</div>
-        <div>{{ selectedHospitalData.ed[0].expecting }}</div>
-        <div>Clearing Estimate:</div>
-        <div>{{ selectedHospitalData.ambo[0].time }} min</div>
+    <div class="highlights">
+      <div>Emergency Capacity:</div>
+      <div>Emergency Utilization:</div>
+      <div>Expecting Ambulance:</div>
+      <div>Clearing Estimate:</div>
+      <div>{{ selectedHospitalData.ed[0].capacity }}</div>
+
+      <div>
+        {{
+          (
+            (Number(selectedHospitalData.ed[0].patients) /
+              Number(selectedHospitalData.ed[0].capacity)) *
+            100
+          ).toFixed(2)
+        }}%
       </div>
+      <div>{{ selectedHospitalData.ed[0].expecting }}</div>
+
+      <div>{{ selectedHospitalData.ambo[0].time }} min</div>
     </div>
 
     <div class="live-feed-title">
       Live Feed at {{ selectedHospitalData.name }}
     </div>
+
     <div class="live-feed-container">
       <LiveFeed title="Ambulance Bay" :source="ambulanceBay" />
       <LiveFeed title="Emergency Reception" :source="emergency" />
@@ -52,14 +58,14 @@ import Ambulance from "../Ambulance.vue";
 import LiveFeed from "./LiveFeed.vue";
 import ambulanceBay from "./assets/ambulance_bay.webp";
 import emergency from "./assets/emergency.avif";
+import logo from "@/assets/images/healux_darkfont.png";
 
 const { selectedHospitalData, isDataReady } = storeToRefs(useDashboardStore());
 </script>
 
 <style scoped>
 .container {
-  /* margin: 1rem; */
-  padding: 2rem;
+  padding: 1rem 2rem;
   background-color: #f5f5f5;
   border: 1px solid #ccc;
   border-radius: 10px;
@@ -70,6 +76,13 @@ const { selectedHospitalData, isDataReady } = storeToRefs(useDashboardStore());
   justify-content: space-around;
   align-items: center;
 }
+
+.logo {
+  height: 120px;
+  width: 200px;
+  margin-right: 1rem;
+}
+
 .title {
   font-size: 2rem;
   color: #333;
@@ -82,29 +95,32 @@ const { selectedHospitalData, isDataReady } = storeToRefs(useDashboardStore());
 }
 
 .highlights {
-  padding: 2rem;
+  margin: 1rem auto;
+  padding: 1rem;
   display: grid;
-  grid-template-columns: repeat(2, minmax(200px, 1fr));
+  grid-template-columns: repeat(4, minmax(200px, 1fr));
   gap: 1rem;
+  background-color: #e3f2fd; /* New background color */
+  width: 80%;
 }
 
 .highlights > div {
-  background: #e7f1ff;
-  padding: 0.3rem;
+  padding: 0.5rem;
   border-radius: 5px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  color: #333;
+  color: #333; /* Adjusted for readability */
 }
 
-.highlights > div:nth-child(2n) {
-  background-color: bisque;
+.highlights > div:nth-last-child(-n + 4) {
+  background-color: #fff3e0; /* Complementary color for alternating rows */
 }
 
-.live-feed-container {
+.live-feed-container,
+.dashboard-container {
   display: grid;
   grid-template-columns: repeat(2, minmax(300px, 1fr));
   gap: 1rem;
-  margin-top: 2rem;
+  margin-top: 1rem;
 }
 
 .dashboard-container {
