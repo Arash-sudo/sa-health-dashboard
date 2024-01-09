1a44d9e5175e91564eb9153c444a4f16bb3a5816
Arash-sudo <155499806+Arash-sudo@users.noreply.github.com>
2024-01-09 11:37:03 +1030

    testing the website using npm

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+      host: "10.1.1.111",
+      port: "3001",
   }
 })
